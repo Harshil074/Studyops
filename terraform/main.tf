bb90a1962b1321822f1f4e8baebd3464ae845e76
Harshil Bhardwaj <harshilbhardwaj74.in@gmail.com>
2026-07-18 14:33:06 +0530

    Add Terraform bastion module (EC2 + security group, SSH restricted to single IP)

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,12 @@ module "eks" {
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnet_ids
 }
+
+module "bastion" {
+  source = "./modules/bastion"
+
+  vpc_id           = module.vpc.vpc_id
+  subnet_id        = module.vpc.public_subnet_ids[0]
+  key_name         = "studyops-bastion-key"
+  allowed_ssh_cidr = "49.43.107.113/32"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -29,3 +29,7 @@ output "eks_cluster_name" {
 output "eks_cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
+
+output "bastion_public_ip" {
+  value = module.bastion.public_ip
+}
